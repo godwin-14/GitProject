1a16446c7310685b0e64c2a27d0c0dad953b8c18
godwin-14 <godwin.kanagaraj@botree.co.in>
2025-12-29 15:57:30 +0530

    commit from dev

--- src/main/java/com/botree/Main.java
+++ src/main/java/com/botree/Main.java
@@ -3,8 +3,8 @@ package com.botree;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("hello");
+		System.out.println("hello botree");
 	}
 
 }
